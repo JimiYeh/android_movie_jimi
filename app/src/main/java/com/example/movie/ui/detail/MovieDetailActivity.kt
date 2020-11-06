dc4424531f889e0dd50dbbdad7983d37eba528c8
T_Jimi.Ye <jimi.ye@criltds.com>
2020-11-06 14:27:52 +0800

    modify movie detail page UI

--- app/src/main/java/com/example/movie/ui/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/example/movie/ui/detail/MovieDetailActivity.kt
@@ -2,13 +2,14 @@ package com.example.movie.ui.detail
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.marginEnd
 import androidx.core.view.setPadding
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
@@ -22,9 +23,7 @@ import kotlinx.android.synthetic.main.activity_movie_detail.*
 import kotlinx.android.synthetic.main.movie_detail_header.*
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.getViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
-import java.lang.RuntimeException
 
 
 class MovieDetailActivity : AppCompatActivity() {
@@ -44,6 +43,10 @@ class MovieDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
 
         setContentView(R.layout.activity_movie_detail)
 
