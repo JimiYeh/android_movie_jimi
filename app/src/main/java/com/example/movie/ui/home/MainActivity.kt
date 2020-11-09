17f6cab361318ddfd97ed4f16b21c153bba6f4a0
T_Jimi.Ye <jimi.ye@criltds.com>
2020-11-09 14:14:30 +0800

    modify tab item size

--- app/src/main/java/com/example/movie/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/movie/ui/home/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.movie.ui.home
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movie.R
 import com.example.movie.ui.home.adapter.CategoryPageAdapter
 import com.example.movie.ui.home.vo.MovieCategory
+import com.example.movie.util.dpToPixel
+import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_movie_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -30,12 +33,33 @@ class MainActivity : AppCompatActivity() {
                 categoryList)
 
 
+        movieCategoryTabLayout.addOnTabSelectedListener(
+            object: TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    val title = tab?.customView as? TextView
+                    title?.textSize = dpToPixel(this@MainActivity, 16f)
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+                    val title = tab?.customView as? TextView
+                    title?.textSize = dpToPixel(this@MainActivity, 10f)
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+
+                }
+
+            }
+        )
+
         TabLayoutMediator(movieCategoryTabLayout, movieCategoryViewPager) { tab, position ->
-            tab.text = getString(categoryList[position].strResId)
+            val title = TextView(this).apply {
+                text = getString(categoryList[position].strResId)
+            }
+            tab.customView = title
         }.attach()
 
 
     }
-
-
 }
--- app/src/main/java/com/example/movie/util/Util.kt
+++ app/src/main/java/com/example/movie/util/Util.kt
@@ -1,5 +1,7 @@
 package com.example.movie.util
 
+import android.content.Context
+import android.util.DisplayMetrics
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -9,3 +11,5 @@ fun Number.toDecimalFormat(format: String = "0.0"): String = DecimalFormat(forma
 fun Date.toFormat(format: String = "yyyy-MM-dd HH:MM:ss"): String = SimpleDateFormat(format, Locale.getDefault()).format(this)
 
 fun String.toDate(format: String = "yyyy-MM-dd HH:MM:ss"): Date? = SimpleDateFormat(format, Locale.getDefault()).parse(this)
+
+fun dpToPixel(context: Context, dp: Float) = dp * (context.resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT)
